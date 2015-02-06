0c51676790815284188bf157413d106aa9a3c017
Lorenzo Porzi <duck.soup.87@gmail.com>
2015-02-06 13:14:15 +0100

    Correct some comments

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// A simple wrapper for Panoramio image query and download APIs.
+// A simple wrapper for the Panoramio Data API.
 // Panoramio images can be queried by specifying a latitude-longitude boundary
 // and selecting a source dataset. The information is fetched in a concurrent
 // way from the Panoramio website.
